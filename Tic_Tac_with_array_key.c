26633ad04dfa13f3663c9f1b234a076f82d12449
MajoMajo <100646287+Pondelok@users.noreply.github.com>
2022-04-03 10:25:34 +0200

    Update Tic_Tac_with_array_key.c

--- Tic_Tac_with_array_key.c
+++ Tic_Tac_with_array_key.c
@@ -1,106 +1,128 @@
 #include <stdio.h>
 
+char arr[3][3];
+int counter = 49;
+int players = 2;
+int key[6] = {0,0,0,1,0,2};
+
+int sWitchTwo(int player);
+void playTable();
+void swap();
+void enigma();
+
 int main(){
 
-    char arr[3][3];
-    int counter = 49;
-    int players = 2;
-    char player1;
-    char player2;
-    int god[6] = {0,0,0,1,0,2};
-
-    printf("-------------\n");
-    for (int i = 0; i < 3; i++) {
-        printf("|");
-        for (int j = 0; j < 3; j++) {
-            printf(" %c |",arr[i][j] = (char)counter++);
-        }   
-        printf("\n");
-    }       
-    printf("-------------\n"); counter = 49;
+    playTable();
 
    while (1){
 
-        if((players++)%2 == 0){
-            
-            printf("Player1 Enter 1-9 or space to give up\n");
-            scanf("%c", &player1);
-            
-            if(player1 == ' '){
-                printf("\nPlayer1 gave up\n");
-                break;
-            }else{
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if((counter++)==player1){
-                            arr[i][j] = 'X';
-                        }
-                    }
-                }       
-                counter = 49;
-            }
-        }else{
-            
-            printf("Player2 Enter 1-9 or space to give up\n");
-            scanf("%c", &player2);
-            
-            if(player2 == ' '){
-                printf("\nPlayer2 gave up\n");
-                break;
-            }else{
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if ((counter++) == player2) {
-                            arr[i][j] = (char)254;
-                        }
-                    }
-                }        
-                counter = 49;
-            }
-        }
+       ((players++) % 2 == 0) ? players = sWitchTwo(0) : sWitchTwo(1);
 
-       for (int i = 0,a = 0; i < 8; i++) {
-           if (i < 3) { 
-               god[0] = god[2] = god[4] = a++; 
-           }
+       playTable();
 
-           if (i == 3) {
-               for (int j = 0; j < 4; ++j) {
-                   int tmp = god[j]; 
-                   god[j] = god[j + 1]; 
-                   god[j + 1] = tmp;
-               } 
-               a = 2;
-           }
+       enigma();
 
-           if (i > 3 && i < 6) { god[1] = god[3] = god[5] = --a; }
-           if (i == 6) { 
-               god[3] = 1; 
-               god[5] = 2;
-           }
-           if (i == 7) { 
-               god[0] = 2; 
-               god[4] = 0;
-           }
-
-           if (arr[god[0]][god[1]] == arr[god[2]][god[3]] && arr[god[2]][god[3]] == arr[god[4]][god[5]]) {
-               printf("\nYOU are W I N N E R\n");
-               players = 0;
-           }
-       }
-       fflush(stdin);
        if(players == 0)break;
    }
 
-    printf("_____________\n");
+    return 0;
+}
+
+int sWitchTwo(player){
+
+    char choice;
+    char *name[] = {"Player1","Player2" };
+
+    printf("%s Enter 1-9 or space to give up\n",name[player] );
+    scanf("%c", &choice);
+    if(choice == ' '){
+        printf("\n%s gave up\n", name[player]);
+        return players = 0;
+    }else{
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ((counter++) == choice) {
+                    (player == 0) ? arr[i][j] = 'X' : (arr[i][j] = (char)254);
+                }
+            }
+        }
+        counter = 49;
+    }
+    fflush(stdin);
+    return players;
+}
+
+void playTable(){
+
+    printf("-------------\n");
     for (int i = 0; i < 3; i++) {
         printf("|");
         for (int j = 0; j < 3; j++) {
-            printf(" %c |",arr[i][j]);
-        }   
+            printf(" %c |", (players != 2) ? arr[i][j] : (arr[i][j] = (char)counter++) );
+        }
         printf("\n");
-    }       
+    }
     printf("-------------\n");
-
-    return 0;
+    counter = 49;
 }
+
+void swap(){
+    for (int j = 0; j < 6; ++j) {
+        int tmp = key[j];
+        key[j] = key[j + 1];
+        key[j + 1] = tmp;
+        j++;
+    }
+}
+
+void enigma(){
+
+    for (int i = 0,a = 0; i < 8; i++) {
+
+        if (i < 3) {key[0] = key[2] = key[4] = a++;}
+        if (i == 3) { swap(); a = 2;}
+        if (i > 3 && i < 6) {key[1] = key[3] = key[5] = --a;}
+        if (i == 6) { key[3] = 1; key[5] = 2;}
+        if (i == 7) { key[0] = 2; key[4] = 0;}
+
+        if (arr[key[0]][key[1]] == arr[key[2]][key[3]] && arr[key[2]][key[3]] == arr[key[4]][key[5]]) {
+            printf("\nYOU ARE W I N N E R ! ! ! \n");
+            players = 0;
+        }
+    }
+}
+
+//
+//            Enigma output  in loop ->
+//            0 0 - 0 1 - 0 2
+//            winner ?
+//            1 0 - 1 1 - 1 2
+//            winner ?
+//            2 0 - 2 1 - 2 2
+//            winner ?
+//            0 2 - 1 2 - 2 2
+//            winner ?
+//            0 1 - 1 1 - 2 1
+//            winner ?
+//            0 0 - 1 0 - 2 0
+//            winner ?
+//            0 0 - 1 1 - 2 2
+//            winner ?
+//            2 0 - 1 1 - 0 2
+//            winner ?
+//
+//
+//
+//            0 0 - 0 1 - 0 2
+//            1 0 - 1 1 - 1 2
+//            2 0 - 2 1 - 2 2
+//            0 2 - 1 2 - 2 2
+//            0 1 - 1 1 - 2 1
+//            0 0 - 1 0 - 2 0
+//            0 0 - 1 1 - 2 2
+//            2 0 - 1 1 - 0 2
+//
+//            array positions ->
+//            00 - 01 - 02
+//            10 - 11 - 12
+//            20 - 21 - 22
